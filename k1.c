408b52613304b0764c3e6d0425bc04b6d2d2a8e6
jeyapuspha <jeyapushpa98@gmail.com>
2016-07-07 19:33:59 +0530

    Create k1.c

---
+++ k1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+scanf("%d",&n);
+if(n>0)
+{
+printf("+ number");
+}
+else
+if(n==0)
+{
+printf(" 0\n");
+}
+else
+{
+printf("- number\n");
+}
+getch();
+}
